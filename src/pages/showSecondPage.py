807938216dc73b8e8e6e0b0de0fddbd7ea4adb93
Nayeli Montiel <133909181+Naye013@users.noreply.github.com>
2024-03-04 18:17:29 -0800

    Update showSecondPage.py

--- src/pages/showSecondPage.py
+++ src/pages/showSecondPage.py
@@ -9,7 +9,7 @@ import dash_vega_components as dvc
 alt.data_transformers.enable("vegafusion")
 dash.register_page(__name__, path='/showSecondPage.py', name="Causes of WildFire")
 # Load data
-df = pd.read_csv("../data/processed/output.csv", low_memory=False)
+df = pd.read_csv("../src/output.csv", low_memory=False)
 alt.themes.enable('dark')
 wildfires_causes = df.groupby(['FIRE_YEAR', 'state_descriptions', 'FIRE_SIZE_CLASS', 'STAT_CAUSE_DESCR']).size().reset_index(name='COUNT')
 # Function to create Altair chart
